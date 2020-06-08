c664cd84ab9a26c01abd2ddac45479732fda0831
Park June Chul <pjc0247@naver.com>
2020-06-09 02:15:32 +0900

    update

--- src/render/render.ts
+++ src/render/render.ts
@@ -4,11 +4,6 @@ import ContextStorage from "../hook/contextStorage";
 import Postworks from "./postworks";
 import VDom from "./vdom";
 
-class ReactContext {
-  rootContextStorage: ContextStorage;
-  contextStorage: ContextStorage;
-}
-
 let rctx: RenderContext = null;
 let cs: ContextStorage = null;
 export const grctx = () => rctx; // 이름이 이상함
@@ -52,7 +47,6 @@ const runRenderTask = (fn): VDom => {
   return computed;
 };
 const renderComponentReal = (_rctx: RenderContext, fn: (...any) => VDom | string, postworks: Postworks) => {
-  console.log('RenderReal', _rctx);
   cs = _rctx.cs;
   const _cs = cs;
   cs.reset();
